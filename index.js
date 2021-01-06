6329eb3952b543a5723a568d978b49a642229157
aligilan <ali.gilan.ag@gmail.com>
2021-01-06 09:48:08 +0330

    Add show, hide methods

--- index.js
+++ index.js
@@ -1,53 +1,71 @@
 'use strict'
 
 module.exports = class Loading {
-    loading_by_selector(){
-        this.target = document.querySelector(this.target)
-        this.manage_loading(this.target)
-    }
-
-    manage_loading(target_element){
-        if(!target_element) return ;
-
-        // Disable button on loading
-        this.manage_button(target_element)
-
-        if(!target_element.classList.contains('overlay-container')){
-            target_element.classList.add('overlay-container');
-
-            let overlay_node = document.createElement('div');
-            overlay_node.classList.add('my-overlay');
-            let spinner_node = document.createElement('div');
-            spinner_node.classList.add('spinner');
-            overlay_node.appendChild(spinner_node);
-            target_element.appendChild(overlay_node);
-
+    manage_loading(){
+        //set target-element
+        if(typeof this.target === "object"){
+            this.target_element = this.target;
+        }else if(typeof this.target === 'string'){
+            this.target_element = document.querySelector(this.target);
         }else{
-            target_element.classList.remove('overlay-container');
-            let my_overlay = target_element.getElementsByClassName('my-overlay')[0];
-
-            if(!!my_overlay)
-                my_overlay.remove();
+            console.log('Show loading Error: target type not support ....')
+        }
+        
+        // Disable button on loading
+        this.manage_button()
+    }
+    
+    toggle_loading(){
+        this.manage_loading();
+        
+        if(!this.target_element.classList.contains('overlay-container')){
+            this.show_loading();
+        }else{
+            this.hide_loading();
         }
     }
-
-    manage_button(target_element){
-        if(!target_element) return ;
-
-        if(target_element.nodeName == 'button' || target_element.type == 'submit'){
-            target_element.disabled = !!!target_element.classList.contains('overlay-container')
+    show_loading(){
+        this.manage_loading();
+        if(!this.target_element) return;
+        this.target_element.classList.add('overlay-container');
+        let overlay_node = document.createElement('div');
+        overlay_node.classList.add('my-overlay');
+        let spinner_node = document.createElement('div');
+        spinner_node.classList.add('spinner');
+        overlay_node.appendChild(spinner_node);
+        this.target_element.appendChild(overlay_node);
+    }
+    hide_loading(){
+        this.manage_loading();
+        if(!this.target_element) return;
+        this.target_element.classList.remove('overlay-container');
+        let my_overlay = this.target_element.getElementsByClassName('my-overlay')[0];
+        
+        if(!!my_overlay){
+            my_overlay.remove();
         }
     }
-
+    
+    manage_button(){
+        if(!this.target_element) return ;
+        
+        if(this.target_element.nodeName === 'button' || this.target_element.type === 'submit'){
+            this.target_element.disabled = !!!this.target_element.classList.contains('overlay-container')
+        }
+    }
+    
     toggle(target){
         this.target = target;
-
-        if(typeof this.target === "object"){
-            this.manage_loading(this.target)
-        }else if(typeof this.target === 'string'){
-            this.loading_by_selector();
-        }else{
-            console.log('Show loading Error: target is type not support ....')
-        }
+        this.toggle_loading();
+    }
+    
+    show(target){
+        this.target = target;
+        this.show_loading();
+    }
+    
+    hide(target){
+        this.target = target;
+        this.hide_loading();
     }
 }
